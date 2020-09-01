3d925957feb9ac8066d64fc800ba5e75d4884ed2
Aman Agrawal <amanagr@zulip.com>
2020-09-01 11:24:40 +0530

    settings: Don't use css classes starting with `icon`.
    Fixes #16252.
    
    icon* classes are used by bootstrap for displaying glyphicons.
    We removed these classes in our custom version of bootstrap 2.1.1;
    but since our reset to v2.3.2, they have been added again and hence
    any classes starting with icon* in zulip will have to be renamed.

--- static/js/settings_org.js
+++ static/js/settings_org.js
@@ -447,7 +447,7 @@ exports.change_save_button_state = function ($element, state) {
     }
 
     const $saveBtn = $element.find(".save-button");
-    const $textEl = $saveBtn.find(".icon-button-text");
+    const $textEl = $saveBtn.find(".custom-settings-button-text");
 
     if (state !== "saving") {
         $saveBtn.removeClass("saving");
