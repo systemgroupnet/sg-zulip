e57bc7600ebc13f92268f3285861cde3ba17e46c
Aryan Ebrahimpour <oxaryan@outlook.com>
2020-09-22 20:00:28 +0330

    Change webhook view decorator

--- zerver/webhooks/azuredevops_sg/view.py
+++ zerver/webhooks/azuredevops_sg/view.py
@@ -3,14 +3,14 @@ from typing import Any, Dict, Iterable, Optional
 from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
 
-from zerver.decorator import api_key_only_webhook_view
+from zerver.decorator import webhook_view
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_error, json_success
 from zerver.lib.validator import check_dict, check_string
 from zerver.models import UserProfile
 
-@api_key_only_webhook_view('AzureDevops_SG')
+@webhook_view('AzureDevops_SG')
 @has_request_variables
 def api_azuredevops_sg_webhook(
         request: HttpRequest, user_profile: UserProfile,
